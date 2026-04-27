eecc0433961c05f297cb157f45667e1806eeef02
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2026-04-27 01:11:02 -0300

    Incorporação do carrosel

---
+++ src/components/carousel/CarouselStructure.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import { motion } from 'framer-motion';
+import './CarouselStructure.css';
+
+export const CarouselStructure = ({ items, onItemClick, categoriaAtiva }) => {
+  // align: 'center' faz com que o item clicado tente ficar no meio
+  const [emblaRef] = useEmblaCarousel({ 
+    align: 'center', 
+    containScroll: 'trimSnaps',
+    dragFree: true 
+  });
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        {items.map((item) => {
+          const isActive = item.id === categoriaAtiva;
+          
+          return (
+            <motion.div 
+              className={`embla__slide ${isActive ? 'is-active' : ''}`} 
+              key={item.id}
+              onClick={() => onItemClick(item.id)}
+              whileTap={{ scale: 0.95 }}
+            >
+              <div className="category-content">
+                 <div className="icon-box">{item.image}</div>
+                 <span className="category-name">{item.name}</span>
+                 <div className={`active-indicator ${isActive ? 'visible' : ''}`}></div>
+              </div>
+            </motion.div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
